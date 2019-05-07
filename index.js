615823a940412864c6d12c1924b1df05a33a5ee6
vikas kodag <vikaskodag2@gmail.com>
2019-05-07 23:46:41 +0530

    edge template engine use kar rahe bhailog

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const path = require('path')
 const express = require('express')
+const edge = require('express-edge');
 
 const app = new express()
 app.use(express.static('public'))
+app.use(edge)
+// app.set('view engine', 'express-edge')
+app.set('views', `${__dirname}/views`)
+
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
 })
 
 
 
-app.listen(5000, () => {
-    console.log('app listening on port 5000.')
+
+app.listen(60000, () => {
+    console.log('app listening on port 60000.')
 })
