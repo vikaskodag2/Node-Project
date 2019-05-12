c42a92e84cc02ba39f5b3d5fabb501443ef54f60
vikas kodag <vikaskodag2@gmail.com>
2019-05-12 23:18:47 +0530

    forms for uploading blog

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const path = require('path')
 const express = require('express')
-const edge = require('express-edge');
+const edge = require('express-edge')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = new express()
+
 app.use(express.static('public'))
 app.use(edge)
-// app.set('view engine', 'express-edge')
 app.set('views', `${__dirname}/views`)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb://localhost/node-js-blog', { useNewUrlParser: true })
 
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.post('/post/store', (req, res) => {
+    res.redirect('/')   
+})
+
+app.get('/post/new', (req, res) => {
+    res.render('create')
 })
 
 app.get('/about', (req, res) => {
